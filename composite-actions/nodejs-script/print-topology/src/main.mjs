3cd080521d020c5a08c7915d15757ea164dcd6c5
Alexandru Marjina <alexandru.marjina.ext@extendaretail.com>
2023-07-18 10:46:07 +0300

    feat(EA-1000): Use print toplogy action from extenda/actions

--- composite-actions/nodejs-script/print-topology/src/main.mjs
+++ composite-actions/nodejs-script/print-topology/src/main.mjs
@@ -1,5 +1,5 @@
 import fs from 'fs';
-const core = require('@actions/core');
+import * as core from '@actions/core';
 
 class SubTopology {
     static pattern = /Sub-topology: ([0-9]*)/;
@@ -161,9 +161,7 @@ const nameFunction = (value) => value.replaceAll('-', '-<br>');
 
 async function generateTopology(){
     try {
-        // const topologyFilePath = process.argv[2] || 'docs/topology/stream.txt';
         const topologyFilePath = core.getInput('topologyFilePath') || 'docs/topology/stream.txt';
-        // const readmeFilePath = process.argv[3] || 'README.md';
         const readmeFilePath = core.getInput('readmeFilePath') || 'README.md';
         // Read the contents of topology/stream.txt file
         const topologyString = fs.readFileSync(topologyFilePath,'utf8')
