1f55789c5a05b4faa7f2e43aa4ca312288f9e680
Raj Ghosh <raj.ghosh@extendaretail.com>
2024-09-26 00:35:24 +0530

    feat(HII-10367): Add shared action for uploading source maps

---
+++ composite-actions/frontend-generic/upload-source-maps/README.md
@@ -0,0 +1,23 @@
+# Deploy upload-source-maps action
+
+This is a shared action for a frontend application: uploading source maps to Elasticsearch.
+
+### Example
+
+```yaml
+- name: Upload source maps
+  uses: extenda/shared-workflows/composite-actions/frontend-generic/upload-source-maps@master
+  with:
+    SERVICE_NAME: "your-service-name"
+    SERVICE_VERSION: "your-service-version"
+    BUILD_DIR: "./dist"
+    SECRET_AUTH: ${{ secrets.SECRET_AUTH }}
+    GCLOUD_AUTH_STAGING: ${{ secrets.GCLOUD_AUTH_STAGING }}
+    ELASTICSEARCH_URL: ${{ secrets.ELASTICSEARCH_URL }}
+    ELASTICSEARCH_API_KEY: ${{ secrets.ELASTICSEARCH_API_KEY }}
+```
+
+### Requirements
+
+- You have to have valid `Dockerfile`, in the root of your project.
+- You have to have valid `cloud-run.yaml` file in the root of your project.
---
+++ composite-actions/frontend-generic/upload-source-maps/scripts/upload-source-maps.js
@@ -0,0 +1,151 @@
+const fs = require("fs");
+const path = require("path");
+
+const BUILD_DIR = process.env.BUILD_DIR ?? `/dist/recon/`;
+const SERVICE_NAME = process.env.SERVICE_NAME ?? "hiiretail-in-store-landing";
+const SERVICE_VERSION = process.env.SERVICE_VERSION ?? "1";
+
+const ELASTICSEARCH_URL = process.env.ELASTICSEARCH_URL;
+const ELASTICSEARCH_API_KEY = process.env.ELASTICSEARCH_API_KEY;
+
+// Helper function to get existing source maps from Elasticsearch
+async function getExistingSourceMaps() {
+  try {
+    const response = await fetch(ELASTICSEARCH_URL, {
+      method: "GET",
+      headers: {
+        Authorization: `ApiKey ${ELASTICSEARCH_API_KEY}`,
+        "Content-Type": "multipart/form-data",
+      },
+    });
+    if (!response.ok)
+      throw new Error(`Failed to fetch source maps: ${response.statusText}`);
+    return await response.json();
+  } catch (error) {
+    throw new Error(`Error fetching source maps: ${error.message}`);
+  }
+}
+
+// Function to get source maps from the build directory
+function getBuildSourceMaps() {
+  try {
+    const filePath = path.join(__dirname, BUILD_DIR);
+    const files = fs.readdirSync(filePath);
+
+    return files
+      .filter((file) => file.endsWith(".map"))
+      .map((file) => ({
+        mapFileName: file,
+        jsFileName: file.replace(".map", ""),
+      }));
+  } catch (error) {
+    console.error(`Error reading build directory: ${error.message}`);
+    process.exit(1);
+  }
+}
+
+// Helper function to delete a source map from Elasticsearch
+async function deleteSourceMaps(sourceMapsToDelete) {
+  const deletePromises = sourceMapsToDelete.map(async (artifactId) => {
+    try {
+      const headers = {
+        Authorization: `ApiKey ${ELASTICSEARCH_API_KEY}`,
+        "KBN-XSRF": "True",
+      };
+      const response = await fetch(`${ELASTICSEARCH_URL}/${artifactId}`, {
+        method: "DELETE",
+        headers: headers,
+      });
+      return {
+        artifactId,
+        success: response.ok,
+        statusText: response.statusText,
+      };
+    } catch (error) {
+      return { artifactId, success: false, error: error.message };
+    }
+  });
+
+  return await Promise.all(deletePromises);
+}
+
+// Helper function to upload a new source map to Elasticsearch
+async function uploadSourceMap(sourceMapsToUpload) {
+  const uploadPromises = sourceMapsToUpload.map(
+    async ({ jsFileName, mapFileName }) => {
+      const formData = new FormData();
+
+      formData.append(
+        "sourcemap",
+        fs.readFileSync(path.join(__dirname, BUILD_DIR, mapFileName))
+      );
+      formData.append("service_version", SERVICE_VERSION);
+      formData.append("bundle_filepath", `/recon/${jsFileName}`);
+      formData.append("service_name", SERVICE_NAME);
+
+      try {
+        const headers = {
+          Authorization: `ApiKey ${ELASTICSEARCH_API_KEY}`,
+          "kbn-xsrf": true,
+        };
+        const response = await fetch(ELASTICSEARCH_URL, {
+          method: "POST",
+          headers: headers,
+          body: formData,
+        });
+        return {
+          mapFileName,
+          success: response.ok,
+          statusText: response.statusText,
+        };
+      } catch (error) {
+        return { mapFileName, success: false, error: error.message };
+      }
+    }
+  );
+
+  return await Promise.all(uploadPromises);
+}
+
+(async function manageSourceMaps() {
+  try {
+    const { artifacts } = await getExistingSourceMaps();
+
+    if (artifacts.length) {
+      const sourceMapsToDelete = artifacts
+        .filter(
+          ({ body }) =>
+            body.serviceName === SERVICE_NAME &&
+            body.serviceVersion === SERVICE_VERSION &&
+            body.bundleFilepath.startsWith("/recon/")
+        )
+        .map(({ id }) => id);
+
+      const deleteResults = await deleteSourceMaps(sourceMapsToDelete);
+      const failedDeletes = deleteResults.filter(({ success }) => !success);
+
+      if (failedDeletes.length) {
+        throw new Error(
+          `Error deleting source maps: ${JSON.stringify(
+            failedDeletes,
+            null,
+            2
+          )}`
+        );
+      }
+    }
+
+    const sourceMapsToUpload = getBuildSourceMaps();
+    const uploadResults = await uploadSourceMap(sourceMapsToUpload);
+    const failedUploads = uploadResults.filter(({ success }) => !success);
+
+    if (failedUploads.length) {
+      throw new Error(
+        `Error uploading source maps: ${JSON.stringify(failedUploads, null, 2)}`
+      );
+    }
+  } catch (error) {
+    console.error(error.message);
+    process.exit(1);
+  }
+})();
